4dd3018915077b82abc994e75fea6e22949aff75
Anita <codingwithanita@gmail.com>
2020-09-12 18:32:05 -0700

    Added Parentheses to  Parameters

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,13 +5,6 @@ const User = require('../../models/User');
 const validateRegisterInput = require('../../validation/register');
 const router = express.Router();
 
-//@route  GET/api/users/test
-//@desc   tests users files
-//@access public
-router.get('/test', (req, res) => res.json({
-  msg: 'User Works'
- }));
-
 //@route  POST/api/users/register
 //@desc   registers the user
 //@access public
