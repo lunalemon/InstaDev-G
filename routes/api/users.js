dde126a85ac1705948b02c78adf4182fbd73cbec
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2020-09-13 22:38:03 -0700

    find email and password check

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,7 +16,7 @@ router.post('/register', (req, res) => {
   } 
 
   User.findOne({
-    email:req.body.email
+    email: req.body.email,
   })
   .then((user) => {
     if (user) {
@@ -34,19 +34,20 @@ router.post('/register', (req, res) => {
     password: req.body.password
   });
 
-  bcrypt.genSalt(10, (err, salt) => {
-    if(err) throw err;
-    bcrypt.hash(newUser.password, salt, (err, hash) => {
-      if(err) throw err;
-      newUser.password = hash;
-      newUser.save()
-      .then((user) => res.json(user))
-      .catch((err) => console.log(err))
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
+          });
+        });
+      }
     })
-  })
-  }
-})
-.catch((err) => console.log(err));
+    .catch((err) => console.log(err));
 })
 
 //FOR VEENA <3
@@ -59,7 +60,26 @@ router.post('/login', (req,res)=>{
 const email=req.body.email;
 const password=req.body.password;
 
+//Find a user with the given email
 
+User.findOne({ email: email })
+  .then((user) => {
+    if (!user) {
+      return res.status(404).json({ email: " User not found" });
+    }
+
+    //check for password
+    bcrypt.compare(password, user.password) // comparing new password with hashed password
+      .then((isMatch) => {
+        if (isMatch) {
+          return res.json({ msg: "Success!" });
+        } else {
+          return res.status(400).json({ password: "Password incorrect" });
+        }
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 }
 )
