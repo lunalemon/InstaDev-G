933f9142078b4a2bc13535e0b888836d6e5087b0
Veena Krishna Sandana <veenakrishna.s@gmail.com>
2020-09-13 22:38:03 -0700

    find email and password check

--- routes/api/users.js
+++ routes/api/users.js
@@ -10,37 +10,38 @@ const router = express.Router();
 //@access public
 router.post('/register', (req, res) => {
   User.findOne({
-    email:req.body.email
-  })
-  .then(user => {
-    if (user) {
-    return res.status(400).json({email: 'Email Already Exists'});
-  } else {
-    const avatar = gravatar.url(req.body.email, {
-    s:'200',
-    r:'pg',
-    d:'mm'
-  });
-  const newUser = new User({
-    name: req.body.name,
     email: req.body.email,
-    avatar: avatar,
-    password: req.body.password
-  });
-
-  bcrypt.genSalt(10, (err, salt) => {
-    if(err) throw err;
-    bcrypt.hash(newUser.password, salt, (err, hash) => {
-      if(err) throw err;
-      newUser.password = hash;
-      newUser.save()
-      .then(user => res.json(user))
-      .catch(err => console.log(err))
-    })
   })
-  }
-})
-.catch();
+    .then((user) => {
+      if (user) {
+        return res.status(400).json({ email: "Email Already Exists" });
+      } else {
+        const avatar = gravatar.url(req.body.email, {
+          s: "200",
+          r: "pg",
+          d: "mm",
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar: avatar,
+          password: req.body.password,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) throw err;
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    })
+    .catch((err) => console.log(err));
 })
 
 //FOR VEENA <3
@@ -53,7 +54,26 @@ router.post('/login', (req,res)=>{
 const email=req.body.email;
 const password=req.body.password;
 
+//Find a user with the given email
 
+User.findOne({ email: email })
+  .then((user) => {
+    if (!user) {
+      return res.status(404).json({ email: " User not found" });
+    }
+
+    //check for password
+    bcrypt.compare(password, user.password) // comparing new password with hashed password
+      .then((isMatch) => {
+        if (isMatch) {
+          return res.json({ msg: "Success!" });
+        } else {
+          return res.status(400).json({ password: "Password incorrect" });
+        }
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 }
 )
