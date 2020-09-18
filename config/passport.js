a76cea69aa5de73ec8bcd0b94307a6754e13d3b5
shanthisravana <shanthi.sravana@gmail.com>
2020-09-17 18:26:03 -0700

    Rebase after Amrutha's profile push

--- config/passport.js
+++ config/passport.js
@@ -1,28 +1,4 @@
 const JwtStrategy = require('passport-jwt').Strategy;
-<<<<<<< HEAD
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const keys = require('../config/keys');
-const User = require('../models/User');
-
-const opts = {};
-
-
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
-
-module.exports = passport => {
-  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-    User.findById(jwt_payload.id)
-      .then(user => {
-        if (user) {
-          return done(null, user);
-        }
-        return done(null, false);
-      })
-      .catch();
-  }));
-}
-=======
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const keys = require('../config/keys');
 const User = require('../models/User');
@@ -47,4 +23,3 @@ module.exports = (passport) => {
   );
 };
 
->>>>>>> e42db258b9504482f4dac83ad29521c1ebfcd96c
