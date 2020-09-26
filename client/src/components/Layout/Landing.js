8e8582d3827e7d087c1ee957dccf7d82acd250ac
amruthahr31 <36977138+amruthahr31@users.noreply.github.com>
2020-09-25 18:20:28 -0700

    Landing Page Finalised

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -7,10 +7,10 @@ export default class Landing extends Component {
     <div className="dark-overlay landing-inner text-light">
       <div className="container">
         <div className="row">
-          <div className="col-md-12 text-center">
+          <div className="col-xl-12 text-center">
             <h1 className="display-3 mb-4">InstaDev-G
             </h1>
-            <p className="lead"> Create a profile, follow/unfollow your interests, share posts and get help from other developers. </p>
+            <p className="lead"> Passion. Code. Collaboration. </p>
             
   
                 <a href="login.html" className="btn btn-lg btn-light">Login</a>
