b0290243a0649489cfa2162af552d23826c0a7c3
Anita <codingwithanita@gmail.com>
2020-10-04 20:30:43 -0700

    Redux 'Reducers' and 'Actions'

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
 import './App.css';
 import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Footer from './components/Layout/Footer';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Register from './components/auth/Register';
+import store from './store';
 
-function App() {
+class App extends Component {
+  render() {
   return (
+    <Provider store={store}>
     <Router>
     <div className="App">
     <Navbar />
@@ -16,7 +20,9 @@ function App() {
     <Footer />
     </div>
     </Router>
+    </Provider>
   );
+};
 }
 
 export default App;
